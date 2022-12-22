367161c85189da367ae5b1487441de3138b7332b
Protoxos <ptx.devs@gmail.com>
2022-12-22 13:12:28 -0600

    Initial commit

---
+++ InjectControllerTest/Controllers/InjectController.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using System.Web.Mvc;
+
+namespace InjectControllerTest.Controllers
+{
+    public class InjectController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("<h1>Hello mundo!</h1>");
+        }
+        string LoadDrives()
+        {
+            string res = "";
+            var drives = Directory.GetLogicalDrives();
+            foreach (var drive in drives)
+            {
+                DriveInfo d = new DriveInfo(drive);
+                if (d.IsReady)
+                {
+                    res += "<br>" + d.Name;
+                    res += LoadDir(d.Name + "\\", 1);
+                }
+            }
+            return res;
+        }
+        public string space(int level)
+        {
+            string res = "";
+            level = level * 4;
+            for (int i = 0; i < level; i++)
+                res += "&nbsp;";
+            return res;
+        }
+        private string LoadDir(string path, int level)
+        {
+            string res = "";
+            try
+            {
+                string[] folders = Directory.GetDirectories(path);
+
+                foreach (var sub in folders)
+                {
+                    DirectoryInfo di = new DirectoryInfo(sub);
+                    res += "<br>" + space(level) + di.Name;
+                    res += LoadDir(di.Name + "\\", level + 1);
+                }
+            }
+            catch { }
+            return res;
+        }
+
+    }
+}
---
+++ InjectControllerTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InjectControllerTest.MvcApplication" Language="C#" %>
